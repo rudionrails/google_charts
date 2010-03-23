3386d61c627e19bb0f11ed0cc3467fac2e135c84
Rudolf Schmidt <me@rudionrails.com>
2010-03-23 15:17:04 +0100

    added initial PieChart

---
+++ init.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/lib/awesome_chart'
+
+if defined?( ActionView::Base )
+  ActionView::Base.send :include, Rudionrails::AwesomeChart
+else
+  $stderr.puts "Skipping AwesomeChart plugin. `gem install actionpack` and try again."
+end
